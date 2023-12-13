cb0b60f544c2db121477c1b999c91a315a488478
Geeta <geetakush3@gmail.com>
2023-12-13 05:45:54 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,15 @@
+function sum(){
+    var add = 2+2;
+    console.log("this is two no add"+add);
+    console.log(this);
+
+}
+sum();
+
+
+var i=5;
+console.log(i)
+
+
+var text=10
+console.log(typeof(text))
